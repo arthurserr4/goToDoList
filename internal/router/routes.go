cdd8a65c52f661585b8edd90206a23553a2ac84c
arthurserr4 <arthurserra01@gmail.com>
2025-03-23 20:19:39 -0300

    adding the routes

---
+++ internal/router/routes.go
@@ -0,0 +1,26 @@
+package router
+
+import (
+	docs "github.com/arthurserr4/goToDoList/docs"
+	"github.com/arthurserr4/goToDoList/internal/handler"
+	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+func initializeRoutes(router *gin.Engine) {
+	// Initialize handler
+	handler.InitializeHandler()
+	basePath := "/api/v1"
+	docs.SwaggerInfo.BasePath = basePath
+	v1 := router.Group(basePath)
+	{
+		v1.POST("/todo", handler.CreateTodoHandler)
+		v1.GET("/todo", handler.CreateTodoHandler)
+		v1.PUT("/todo/:id", handler.UpdateTodoHandler)
+		v1.DELETE("/todo/:id", handler.DeleteTodoHandler)
+		v1.GET("/todos", handler.ListTodosHandler)
+	}
+	// Initialize Swagger
+	router.GET("swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+}
